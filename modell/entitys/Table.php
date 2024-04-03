8ce9f04aad3352d2cb636a1c4543a6138ba5540a
lazaro13251 <lazaro13251@gmail.com>
2024-04-02 21:45:57 -0600

    Creacion de la clase para conectar la base de datos y inicio de la clase tabla

---
+++ modell/entitys/Table.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Descripcion de Table:
+ * 
+ * @author L.S.C Fernando Garcia Lazaro {
+ *  Desarrollador BacKend
+ *  Móvil -> (+52) 222-819-4156
+ *  Email -> lazaro13251@gmail.com
+ * } -> GitHub: https://github.com/lazaro13251/upload-csv
+ * 
+ * @copyright upload-csv (c) 2023 - 2024
+ * 
+ */
+
+namespace Modell\Entitys\Table;
+
+class Table {
+    //put your code here
+}
+
+?>
